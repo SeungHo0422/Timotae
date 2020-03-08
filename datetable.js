f255e6a0341bc748cc79cee989b8b7e32008bd88
GYEOM <skqnrua5123@naver.com>
2020-03-09 08:35:22 +0900

    NBK 0309 기부현황 검색 기능 js추가

--- datetable.js
+++ datetable.js
@@ -1,35 +1,44 @@
-jQuery(function($){ 
-    $("#table").DataTable(); 
-}); 
-$("#table").DataTable({
-    // 표시 건수기능 숨기기
-    lengthChange: true,
-    // 검색 기능 숨기기
-    searching: false,
-    // 정렬 기능 숨기기
-    ordering: false
-    ,
-    // 정보 표시 숨기기
+$(document).ready(function () {
 
-    info: true,
-    // 페이징 기능 숨기기
-    paging: true,
+    var table = $("#table").DataTable({
+        responsive: true,
+        orderMulti: true,
+        order : [[1, 'asc']],
+        columns: [
+            {"data": "#"},
+            {"data": "구"},
+            {"data": "장소"}, 
+            {"data": "개수"}
 
-    "language": {
-        "emptyTable": "데이터가 없어요.",
-        "lengthMenu": "페이지당 _MENU_ 개씩 보기",
-        "info": "현재 _START_ - _END_ / _TOTAL_건",
-        "infoEmpty": "데이터 없음",
-        "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
-        "search": "에서 검색: ",
-        "zeroRecords": "일치하는 데이터가 없어요.",
-        "loadingRecords": "로딩중...",
-        "processing":     "잠시만 기다려 주세요...",
-        "paginate": {
-            "next": "다음",
-            "previous": "이전"
-        }
-    }
+        ],
+        "language": {
+            "emptyTable": "데이터가 없어요.",
+            "lengthMenu": "페이지당 _MENU_ 개씩 보기",
+            "info": "현재 _START_ - _END_ / _TOTAL_건",
+            "infoEmpty": "데이터 없음",
+            "infoFiltered": "( _MAX_건의 데이터에서 필터링됨 )",
+            "search": "에서 검색: ",
+            "zeroRecords": "일치하는 데이터가 없어요.",
+            "loadingRecords": "로딩중...",
+            "processing":     "잠시만 기다려 주세요...",
+            "paginate": {
+                "next": "다음",
+                "previous": "이전"
+            }
+        },
+    }); 
+    $('#table_filter').prepend('<select id="select"></select>');
+    $('#table > thead > tr').children().each(function (indexInArray, valueOfElement) { 
+        $('#select').append('<option>'+valueOfElement.innerHTML+'</option>');
+    });
+    
+    $('.dataTables_filter input').unbind().bind('keyup', function () {
+        var colIndex = document.querySelector('#select').selectedIndex;
+        table.column(colIndex).search(this.value).draw();
+    });
+
+
 });
+ 
 
 // datetable.html을 참고해주세요
